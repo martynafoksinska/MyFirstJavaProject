f803c8addbe9d25734c6de4eda262094d7e84fe7
martynafoksinska <martyna.a.foksinska@gmail.com>
2022-09-13 09:17:15 +0200

    Initial commit

---
+++ src/MyArray.java
@@ -0,0 +1,17 @@
+public class MyArray {
+    public static void main(String[] args) {
+        String[] students = {"Janek", "Piotrek", "Jakub"};
+        int[] numbers = {1, 5, 10, 100};
+
+        System.out.println(students[0]);
+
+        students[0] = "Paweł";
+        System.out.println(students[0]);
+
+        System.out.println("There are " + students.length + " students in the class");
+
+        for (int i = 0; i < students.length; i++) {
+            System.out.println(students[i]);
+        }
+    }
+}
