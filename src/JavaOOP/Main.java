d162cbead3d62b33da37c76e86051bf29c35b830
martynafoksinska <martyna.a.foksinska@gmail.com>
2022-09-14 13:57:02 +0200

    Java OOP exercises

--- src/JavaOOP/Main.java
+++ src/JavaOOP/Main.java
@@ -2,10 +2,21 @@ package JavaOOP;
 
 public class Main {
     public static void main(String[] args) {
-        ClassDemo instance = new ClassDemo();
-        instance.printNumbers();
+//        ClassDemo instance = new ClassDemo();
+//        instance.printNumbers();
+//
+//        ClassDemo instance2 = new ClassDemo();
+//        instance2.printNumbers();
 
-        ClassDemo instance2 = new ClassDemo();
-        instance2.printNumbers();
+        Lamp kitchenLamp = new Lamp("Kitchen");
+        Lamp bedroomLamp = new Lamp("Bedroom");
+
+        kitchenLamp.printLightStatus();
+        bedroomLamp.printLightStatus();
+
+        kitchenLamp.lightStatus = true;
+
+        kitchenLamp.printLightStatus();
+        bedroomLamp.printLightStatus();
     }
 }
