62e8d515cc452fbce0a8a523d6c19827979fcda2
martynafoksinska <martyna.a.foksinska@gmail.com>
2022-09-14 14:59:05 +0200

    Java OOP

--- src/JavaOOP/ClassDemo.java
+++ src/JavaOOP/ClassDemo.java
@@ -2,9 +2,14 @@ package JavaOOP;
 
 public class ClassDemo {
 
-    private int number = 5;
+    public int number = 5;
+    public static String text = "some text";
 
     public void printNumbers() {
         System.out.println("The number is " + number + ".");
+    }
+
+    public void printText() {
+        System.out.println("The text is " + text + ".");
     }
 }
--- src/JavaOOP/Main.java
+++ src/JavaOOP/Main.java
@@ -2,21 +2,26 @@ package JavaOOP;
 
 public class Main {
     public static void main(String[] args) {
-//        ClassDemo instance = new ClassDemo();
-//        instance.printNumbers();
+
+        ClassDemo instance = new ClassDemo();
+        instance.number = 10;
+        instance.printNumbers();
+        instance.printText();
+
+        ClassDemo instance2 = new ClassDemo();
+        instance2.text = "new text";
+        instance2.printNumbers();
+        instance2.printText();
+
+//        Lamp kitchenLamp = new Lamp("Kitchen");
+//        Lamp bedroomLamp = new Lamp("Bedroom");
 //
-//        ClassDemo instance2 = new ClassDemo();
-//        instance2.printNumbers();
-
-        Lamp kitchenLamp = new Lamp("Kitchen");
-        Lamp bedroomLamp = new Lamp("Bedroom");
-
-        kitchenLamp.printLightStatus();
-        bedroomLamp.printLightStatus();
-
-        kitchenLamp.lightStatus = true;
-
-        kitchenLamp.printLightStatus();
-        bedroomLamp.printLightStatus();
+//        kitchenLamp.printLightStatus();
+//        bedroomLamp.printLightStatus();
+//
+//        kitchenLamp.lightStatus = true;
+//
+//        kitchenLamp.printLightStatus();
+//        bedroomLamp.printLightStatus();
     }
 }
