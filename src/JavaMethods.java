0adf1b5132723ba1ed06e8e2f39cdbf2e4baff3b
martynafoksinska <martyna.a.foksinska@gmail.com>
2022-09-14 12:42:34 +0200

    Java methods done

--- src/JavaMethods.java
+++ src/JavaMethods.java
@@ -4,7 +4,10 @@ public class JavaMethods {
         helloMartyna();
         printHello("Paweł");
         getSum(2, 4);
+        getSum(3.5, 4.5);
+        getSum(3, 4, 5);
         getNameAndAge("Martyna", 26);
+        System.out.println(square(2));
     }
 
     private static void helloWorld() {
@@ -24,9 +27,23 @@ public class JavaMethods {
         System.out.println(sum);
     }
 
-    // Create a method that prints name nad age of a person
+    private static void getSum(double a, double b) {
+        double sum = a + b;
+        System.out.println(sum);
+    }
 
+    private static void getSum(int a, int b, int c) {
+        double sum = a + b + c;
+        System.out.println(sum);
+    }
+
+    // Create a method that prints name nad age of a person
     private static void getNameAndAge(String name, int age) {
         System.out.println(name + " is " + age + " years old.");
+    }
+
+    private static int square(int number) {
+        int square = number * number;
+        return square;
     }
 }
